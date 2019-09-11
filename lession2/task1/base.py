973d6a15861206379a50d7f29f038a9798949720
SeredinGS <grigorii_seredin@mail.ru>
2019-09-11 14:15:02 +0300

    Classes were separated

--- lession2/task1/base.py
+++ lession2/task1/base.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup as bs
 import re
 
 class requestor:
-    def __init__(self, zagolovok, param):
-        self.headers = zagolovok
-        self.params = param
+    def __init__(self):
+        self.headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.71"}
         print('Успех')
 
     def create_dataframe(self, names, linki, mins, maxs, istochniki):
@@ -16,144 +16,42 @@ class requestor:
                                                                                                      'Мин. зп',
                                                                                                      'Макс. зп',
                                                                                                      'Источник'])
-        dataframe_na_export.to_excel('itogoviy.xlsx', index=False)
         return dataframe_na_export
 
     def export_to_excel(self, dataframe1, dataframe2):
         result = pd.concat([dataframe1, dataframe2])
-        result.to_excel('report.xlsx')
+        result.to_excel('report.xlsx', index=False)
 
-class superjob(requestor):
-    def __init__(self, zagolovok, param):
-        self.web_sj = 'https://www.superjob.ru/vacancy/search/'
-        self.headers = zagolovok
-        self.params = param
-        print('Успех')
-
-    def form_lists(self):
-        url = self.get_body(self.web_sj, self.headers, self.params)
-        links = [url[0:-19] + 'page=' + x for x in num_pages]
-        print(links)
-
-        names = []
-        linki = []
-        mins = []
-        maxs = []
-        istochniki = []
-
-        try:
-            for link in links:
-                name, vlink, min, max, istochnik = self.get_vacs(link)
-                names = names + name
-                linki = linki + vlink
-                mins = mins + min
-                maxs = maxs + max
-                istochniki = istochniki + istochnik
-                # istochniki_ser = np.array(istochniki_ser, istochnik)
-        except AttributeError:
-            print('Больше страниц нет. Отбой!')
-            pass
-        return names,linki,mins,maxs,istochniki
-
-    # Делаем реквест
-    def get_body(self, web_sj, headers, params):
-        page = requests.get(web_sj, headers=headers, params=params)
-        result = page.text
-        # print(result)
-        link = page.url
-        print(link)
-        page.close()
-        return link
-
-    # делаем из неё суп(Супер Жоб)
-    def get_vacs(self,link):
-        list_names = []
-        list_links = []
-        list_minzp = []
-        list_maxzp = []
-        list_sources = []
-        source = 'SuperJob'
-        print(link)
-        page_full = requests.get(link)
-        soup = bs(page_full.text, 'lxml')
-
-        # Выбираем блок/столбец, где располагаются все вакансии
-        block_vacs = soup.find('div', {'style': "display:block"}).findChildren(recursive=False)
-        # pprint(block_vacs)
-        print('--------')
-
-        # Парсим нужные нам значения.
-        # Не забываем о проверке содержимого на пустые множества и None-ы
-
-        for vac in block_vacs:
-            list_sources.append(source)
-            vacansii = vac.findAll('div', {"class": "_2g1F-"})
-            # pprint(vacancii)
-            if len(vacansii) == 0:
-                continue
-            vac_names = vacansii[0].find('a', {'target': "_blank"})
-            if vac_names is None:
-                continue
-            # Ссылка на вакансию
-            vac_link = 'http://www.superjob.ru' + vac_names['href']
-            list_links.append(vac_link)
-            # Имя вакансии
-            pprint(vac_names.getText())
-            vac_name = vac_names.getText()
-            list_names.append(vac_name)
-
-            # В случае c зарплатой наблюдается 3 варианта:
-            # а) Известны мин и макс значения
-            # б) Известнен только мин
-            # в) Не указано
-            # Для этого пришлось вводить блок проверки
-            span = vac.findAll('span', {'class': re.compile('f-test-text-company-item-salary')})
-            # pprint(span)
-            zp = span[0].findChildren()
-            # pprint(zp)
-            if zp == []:
-                zp.append('Не указано')
-                print(zp[0])
-                minzp = zp[0]
-                maxzp = ''
-                list_minzp.append(minzp)
-                list_maxzp.append(maxzp)
-            else:
-                try:
-                    minzp = zp[0].getText().replace('\xa0', '')
-                    maxzp = zp[2].getText().replace('\xa0', '')
-                    print(zp[0].getText(), zp[2].getText())
-                    list_minzp.append(minzp)
-                    list_maxzp.append(maxzp)
-                except IndexError:
-                    minzp = zp[0].getText().replace('\xa0', '')
-                    maxzp = ''
-                    print(zp[0].getText())
-                    list_minzp.append(minzp)
-                    list_maxzp.append(maxzp)
-        return list_names, list_links, list_minzp, list_maxzp, list_sources
-
-class headhunter(requestor):
-    def __init__(self, zagolovok, param):
-        self.web_hh = 'https://hh.ru/search/vacancy'
-        self.headers = zagolovok
-        self.params = param
-        print('Успех')
-
-
+    def letsroll(self):
+        self.params = {**self.params, **self.paramsadditional}
 
 
 if __name__ == '__main__':
+   print('zaglushko')
+   '''
+# Делаем реквест
 
-    name_vac = 'художник'
-    list_num = []
-    pages = list('1,2,3,4,5')
-    num_pages = [pages[x] for x in range(0, len(pages), 2)]
-    print(num_pages)
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.71"}
-    params = {"keywords": name_vac}
+    zaproshh = headhunter(paramshh)
 
+
+    names, linki, mins, maxs, istochniki = zaproshh.form_list_hh(num_pages)
+    reporthh = zaproshh.create_dataframe(names, linki, mins, maxs, istochniki)
+
+    pprint(reporthh.head(25))
+
+'''
+
+'''  
+# Методы НН    
+    zaproshh = headhunter(params)
+    
+    names, linki, mins, maxs, istochniki = zaproshh.form_list(num_pages)
+    reporthh = zaproshh.create_dataframe(names, linki, mins, maxs, istochniki)
+'''
+
+        # istochniki_ser = np.array(istochniki_ser, istochnik)
+'''
+# Методы для супержабы
     zapros = superjob(headers, params)
 
     imena, ssylki, miny, maxy, sourcey = zapros.form_lists()
@@ -162,7 +60,7 @@ if __name__ == '__main__':
 
 
 # TODO: сделать метод для экспорта в эйчтимиэль файл
-    '''
+    
     file = open('output.html','w+', encoding='utf-8')
     file.write(result)
     file.close()
--- lession2/task1/main.py
+++ lession2/task1/main.py
@@ -10,3 +10,45 @@
 # ???
 #
 # Профит
+
+# Подготовка'
+
+import superjob as ssj
+import headhunter as hhr
+
+
+if __name__ == '__main__':
+    name_vac = 'инженер'
+    list_num = []
+    pages = list('1,2,3,4,5')
+    num_pages = [pages[x] for x in range(0, len(pages), 2)]
+    print(num_pages)
+    # params = {"keywords": name_vac}
+    # Делаем реквест
+    sj = ssj.superjob()
+    hh = hhr.headhunter()
+
+    sj.applyparams(name_vac)
+    hh.applyparams(name_vac)
+
+    names, linki, mins, maxs, istochniki = hh.form_list_hh(num_pages)
+    reporthh = hh.create_dataframe(names, linki, mins, maxs, istochniki)
+
+    '''
+    file = open('output.html','w+', encoding='utf-8')
+    file.write(result)
+    file.close()
+    '''
+
+    df_na_export = sj.form_lists(num_pages)
+
+    sj.export_to_excel(df_na_export, reporthh)
+
+    '''
+    # Делаем реквест HH
+    # Подготовка
+    name_vac = 'data'
+    pages = list('1,2,3,4,5')
+    num_pages = [pages[x] for x in range(0, len(pages), 2)]
+    list_num = []
+    '''
