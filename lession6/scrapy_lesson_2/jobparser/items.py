447be40fc749f8c1093d557f465ce38d946af0cd
SeredinGS <grigorii_seredin@mail.ru>
2019-09-29 00:10:14 +0300

    Mission complete

---
+++ lession6/scrapy_lesson_2/jobparser/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobparserItem(scrapy.Item):
+    # Структура единицы информации (item'a) в scrapy
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    salary = scrapy.Field()
+
+class instaposts(scrapy.Item):
+    _id = scrapy.Field()
+    posts = scrapy.Field()
---
+++ lession6/scrapy_lesson_2/jobparser/runner.py
@@ -0,0 +1,26 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from jobparser import settings
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.instagram import InstagramSpider
+
+do_env = join(dirname(__file__), '.env')
+load_dotenv(do_env)
+
+#Загружаем переменные из окружения
+INST_LOGIN = os.getenv('INST_LOGIN')
+INST_PSWRD = os.getenv('INST_PSWRD')
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+   # process.crawl(HhruSpider)
+
+    #Передаем параметры при запуске паука
+    process.crawl(InstagramSpider,['geekbrains','python'],INST_LOGIN,INST_PSWRD)
+    process.start()
