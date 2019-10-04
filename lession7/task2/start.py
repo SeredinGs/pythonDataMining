fd34eae3248dffa06fa975fa0eb693b696a1c9ab
SeredinGS <grigorii_seredin@mail.ru>
2019-10-05 00:11:22 +0300

    Accuracy hardcode solution

--- lession7/task2/start.py
+++ lession7/task2/start.py
@@ -2,32 +2,36 @@
 # 2) Написать программу, которая собирает «Хиты продаж» с сайта техники mvideo и складывает данные в БД.
 # Магазины можно выбрать свои. Главный критерий выбора: динамически загружаемые товары
 
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
-from selenium.webdriver.common.keys import Keys
 from pymongo import MongoClient
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from pprint import pprint
 
+def monga_monga(dictionary):
+    client = MongoClient('localhost', 27017)
+    mongo_base = client.mvideo
+    collection = mongo_base["mvideo"]
+    collection.insert_one(dictionary)
+    print('записал в монгу')
+    return dictionary
 
 driver = webdriver.Firefox(executable_path='.\\geckodriver.exe')
 
 driver.get('https://www.mvideo.ru')
 
+# Ювелирно "прибито гвоздями", но работает. Смысл - нажимать на кнопочки ниже, а не большую кнопку "Вправо".
+# Поскольку меньше выборку сделать не удалось пришлось рассчитывать тайминги для циклов. Под термином
+# "собирает "Хиты продаж"" я понял название лотов, поэтому складываем в базу имя лота
 menu = driver.find_elements_by_css_selector("div.gallery-layout div.section div.gallery-layout.sel-hits-block div.gallery-content.accessories-new div.accessories-carousel-holder.carousel.tabletSwipe a[href^='#']")
-
 i = 0
-
 for knopko in menu:
-    if i < 7:
+    if i < 8:
         sleep(3)
         knopko.click()
-        if i >=2:
-            print('parse blocks')
+        if i >=3:
+            name = driver.find_elements_by_class_name("sel-product-tile-title")
+            for imya in name[0:4]:
+                dict = {"Name of good" : imya.text}
+                monga_monga(dict)
             i += 1
             print(i)
         else:
