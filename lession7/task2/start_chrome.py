da14b64508a58da0168dde6278b5828553f6ea12
SeredinGS <grigorii_seredin@mail.ru>
2019-10-05 11:10:11 +0300

    Added chrome solution

--- lession7/task2/start_chrome.py
+++ lession7/task2/start_chrome.py
@@ -2,21 +2,38 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
+from pymongo import MongoClient
 
 driver = webdriver.Chrome('.\\chromedriver.exe')  # Optional argument, if not specified will search path.
 driver.get('https://www.mvideo.ru')
 
-menu = driver.find_element_by_css_selector("div.gallery-layout div.section div.gallery-layout.sel-hits-block div.gallery-content.accessories-new")
-#sleep(5)
-hidden_submenu = driver.find_element_by_css_selector("a.next-btn.sel-hits-button-next")
 
-driver.execute_script("window.scrollTo(0, 894);")
-actions = ActionChains(driver)
-actions.move_to_element(menu)
-actions.perform()
-sleep(2)
-actions.click(hidden_submenu)
-actions.click(hidden_submenu)
-actions.click(hidden_submenu)
-sleep(2)
-actions.perform()
+def monga_monga(dictionary):
+    client = MongoClient('localhost', 27017)
+    mongo_base = client.mvideo
+    collection = mongo_base["mvideo"]
+    collection.insert_one(dictionary)
+    print('записал в монгу')
+    return dictionary
+
+# Ювелирно "прибито гвоздями", но работает. Смысл - нажимать на кнопочки ниже, а не большую кнопку "Вправо".
+# Поскольку меньше выборку сделать не удалось пришлось рассчитывать тайминги для циклов. Под термином
+# "собирает "Хиты продаж"" я понял название лотов, поэтому складываем в базу имя лота
+menu = driver.find_elements_by_css_selector("div.gallery-layout div.section div.gallery-layout.sel-hits-block div.gallery-content.accessories-new div.accessories-carousel-holder.carousel.tabletSwipe a[href^='#']")
+i = 0
+for knopko in menu:
+    if i < 8:
+        sleep(3)
+        knopko.click()
+        if i >=3:
+            name = driver.find_elements_by_class_name("sel-product-tile-title")
+            for imya in name[0:4]:
+                dict = {"Name of good" : imya.text}
+                monga_monga(dict)
+            i += 1
+            print(i)
+        else:
+            i+=1
+            print(i)
+    else:
+        break
